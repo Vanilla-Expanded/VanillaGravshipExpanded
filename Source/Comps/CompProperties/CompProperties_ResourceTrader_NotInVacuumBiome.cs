248403f2314f0fc7e8370f6963a23d6cf280bcdf
SokyranTheDragon <exploder1531@gmail.com>
2025-08-14 00:22:11 +0200

    Initial oxygen net progress

---
+++ Source/Comps/CompProperties/CompProperties_ResourceTrader_NotInVacuumBiome.cs
@@ -0,0 +1,22 @@
+﻿using PipeSystem;
+using UnityEngine;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompProperties_ResourceTrader_NotInVacuumBiome : CompProperties_ResourceTrader
+{
+    [NoTranslate]
+    public string lowOxygenEnvironmentOverlayPath;
+    [Unsaved]
+    public Material lowOxygenEnvironmentOverlay;
+
+    public CompProperties_ResourceTrader_NotInVacuumBiome() => compClass = typeof(CompResourceTrader_NotInVacuumBiome);
+
+    public override void ResolveReferences(ThingDef parentDef)
+    {
+        base.ResolveReferences(parentDef);
+
+        LongEventHandler.ExecuteWhenFinished(() => lowOxygenEnvironmentOverlay = MaterialPool.MatFrom(lowOxygenEnvironmentOverlayPath, ShaderDatabase.MetaOverlay));
+    }
+}
---
+++ Source/Comps/CompResourceTrader_NotInVacuumBiome.cs
@@ -0,0 +1,43 @@
+﻿using PipeSystem;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompResourceTrader_NotInVacuumBiome : CompResourceTrader
+{
+    // TODO: Move to custom PipeNet itself, which is still WIP
+    private bool noAtmosphere;
+
+    protected new CompProperties_ResourceTrader_NotInVacuumBiome Props => (CompProperties_ResourceTrader_NotInVacuumBiome)props;
+
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        base.PostSpawnSetup(respawningAfterLoad);
+
+        pipeNetOverlayDrawer = parent.Map.GetComponent<PipeNetOverlayDrawer>();
+        noAtmosphere = parent.Map.Biome.inVacuum;
+
+        if (noAtmosphere)
+        {
+            Log.Error($"Toggled pulsing for {parent}, overlay: {Props.lowOxygenEnvironmentOverlay} (null={Props.lowOxygenEnvironmentOverlay == null})");
+            pipeNetOverlayDrawer.TogglePulsing(parent, Props.lowOxygenEnvironmentOverlay, true);
+        }
+    }
+
+    public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
+    {
+        pipeNetOverlayDrawer.TogglePulsing(parent, Props.lowOxygenEnvironmentOverlay, false);
+        noAtmosphere = false;
+
+        base.PostDeSpawn(map, mode);
+    }
+
+    public override string CompInspectStringExtra()
+    {
+        if (noAtmosphere)
+            return $"{"VGSE_DisabledNoAtmosphere".Translate()}\n{base.CompInspectStringExtra()}";
+        return base.CompInspectStringExtra();
+    }
+
+    public override bool CanBeOn() => !noAtmosphere && base.CanBeOn();
+}
