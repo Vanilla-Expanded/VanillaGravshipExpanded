94cce5aa3457a2038078d2f645f0ea1fc1a66245
SokyranTheDragon <exploder1531@gmail.com>
2025-08-20 19:22:37 +0200

    Update pipe nets, add oxyalgae plant, start work on astropurifier

---
+++ Source/Comps/CompFacility_Astropurifier.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompFacility_Astropurifier : CompFacility
+{
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        OnLinkAdded += Notify_LinkAdded;
+        OnLinkRemoved += Notify_LinkRemoved;
+
+        base.PostSpawnSetup(respawningAfterLoad);
+    }
+
+    private void Notify_LinkAdded(CompFacility facility, Thing link) => link.TryGetComp<CompResourceTrader_AstrofuelSynthesizer>()?.Notify_LinkAdded(this);
+
+    private void Notify_LinkRemoved(CompFacility facility, Thing link) => link.TryGetComp<CompResourceTrader_AstrofuelSynthesizer>()?.Notify_LinkRemoved(this);
+}
---
+++ Source/Comps/CompResourceTrader_AstrofuelSynthesizer.cs
@@ -0,0 +1,26 @@
+﻿using PipeSystem;
+
+namespace VanillaGravshipExpanded;
+
+public class CompResourceTrader_AstrofuelSynthesizer : CompResourceTrader
+{
+    public CompFacility_Astropurifier astropurifier;
+
+    public void Notify_LinkAdded(CompFacility_Astropurifier facility)
+    {
+        if (astropurifier == facility)
+            return;
+
+        astropurifier = facility;
+        BaseConsumption = Props.consumptionPerTick / 2f;
+    }
+
+    public void Notify_LinkRemoved(CompFacility_Astropurifier facility)
+    {
+        if (astropurifier != facility)
+            return;
+
+        astropurifier = null;
+        BaseConsumption = Props.consumptionPerTick;
+    }
+}
