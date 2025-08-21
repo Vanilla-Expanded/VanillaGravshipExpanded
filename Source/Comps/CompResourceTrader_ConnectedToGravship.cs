fe4eea1930d5137f5c68c43a23b18323780df3e8
SokyranTheDragon <exploder1531@gmail.com>
2025-08-21 21:14:55 +0200

    Update pipe nets, start work on oxygen reclaimer

---
+++ Source/Comps/CompResourceTrader_ConnectedToGravship.cs
@@ -0,0 +1,46 @@
+﻿using PipeSystem;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompResourceTrader_ConnectedToGravship : CompResourceTrader
+{
+    private CompGravshipFacility facility;
+
+    public override bool CanBeOn()
+    {
+        return base.CanBeOn() && (facility == null || facility.CanBeActive);
+    }
+
+    public override void PostPostMake()
+    {
+        base.PostPostMake();
+        InitializeComps();
+    }
+
+    public override void PostExposeData()
+    {
+        base.PostExposeData();
+        if (Scribe.mode == LoadSaveMode.LoadingVars)
+            InitializeComps();
+    }
+
+    private void InitializeComps()
+    {
+        facility = parent.GetComp<CompGravshipFacility>();
+        
+        // facility.OnLinkAdded += FacilityOnOnLinkAdded;
+        // facility.OnLinkRemoved += FacilityOnOnLinkRemoved;
+    }
+
+    // private void FacilityOnOnLinkRemoved(CompFacility arg1, Thing arg2)
+    // {
+    //     throw new System.NotImplementedException();
+    // }
+    //
+    // private void FacilityOnOnLinkAdded(CompFacility arg1, Thing arg2)
+    // {
+    //     throw new System.NotImplementedException();
+    // }
+}
