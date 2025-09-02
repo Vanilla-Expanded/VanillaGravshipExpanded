82394ecb6f6968ac4e61d9703c4a72fc7e28055b
SokyranTheDragon <exploder1531@gmail.com>
2025-09-02 17:37:18 +0200

    Vehicles can pass through vac barriers

---
+++ Source/HarmonyPatches/VehicleFramework_GenGridVehicles_ImpassableForVehicles_Patch.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch("GenGridVehicles", "ImpassableForVehicles")]
+public static class VehicleFramework_GenGridVehicles_ImpassableForVehicles_Patch
+{
+    private static bool Prepare(MethodBase methodBase) => methodBase != null || ModsConfig.IsActive("SmashPhil.VehicleFramework") || ModsConfig.IsActive("SmashPhil.VehicleFramework_steam");
+
+    private static void Postfix(ThingDef __0, ref bool __result)
+    {
+        if (__result && __0.thingClass.SameOrSubclassOf(typeof(Building_VacBarrier)))
+            __result = false;
+    }
+}
