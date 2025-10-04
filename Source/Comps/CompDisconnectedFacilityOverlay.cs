4fb87d58fee495644d4220a0071814026ddca7dd
SokyranTheDragon <exploder1531@gmail.com>
2025-10-04 20:02:29 +0200

    Add overlays to pilot console variants when disconnected

---
+++ Source/Comps/CompDisconnectedFacilityOverlay.cs
@@ -0,0 +1,59 @@
+﻿using RimWorld;
+using VEF.Graphics;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompDisconnectedFacilityOverlay : ThingComp
+{
+    protected CustomOverlayDrawer overlayDrawer;
+    protected CompFacility facility;
+
+    protected CompProperties_DisconnectedFacilityOverlay Props => (CompProperties_DisconnectedFacilityOverlay)props;
+
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        overlayDrawer = parent.Map.GetComponent<CustomOverlayDrawer>();
+        facility.OnLinkAdded += Notify_LinkAdded;
+        facility.OnLinkRemoved += Notify_LinkRemoved;
+
+        // Handle situation where the facility is linked already
+        if (facility.LinkedBuildings.Count > 0)
+            Notify_LinkAdded(facility, null);
+        else
+            Notify_LinkRemoved(facility, null);
+    }
+
+    public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
+    {
+        overlayDrawer = null;
+        facility.OnLinkAdded -= Notify_LinkAdded;
+        facility.OnLinkRemoved -= Notify_LinkRemoved;
+    }
+
+    public override void PostPostMake()
+    {
+        InitComps();
+    }
+
+    public override void PostExposeData()
+    {
+        InitComps();
+    }
+
+    private void InitComps()
+    {
+        facility = parent.GetComp<CompFacility>();
+    }
+
+    protected virtual void Notify_LinkAdded(CompFacility facility, Thing thing)
+    {
+        overlayDrawer?.Disable(parent, Props.overlayDef);
+    }
+
+    protected virtual void Notify_LinkRemoved(CompFacility facility, Thing thing)
+    {
+        if (facility.LinkedBuildings.Count <= 0)
+            overlayDrawer?.Enable(parent, Props.overlayDef);
+    }
+}
---
+++ Source/Comps/CompProperties/CompProperties_DisconnectedFacilityOverlay.cs
@@ -0,0 +1,11 @@
+﻿using VEF.Graphics;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class CompProperties_DisconnectedFacilityOverlay : CompProperties
+{
+    public CustomOverlayDef overlayDef;
+
+    public CompProperties_DisconnectedFacilityOverlay() => compClass = typeof(CompDisconnectedFacilityOverlay);
+}
