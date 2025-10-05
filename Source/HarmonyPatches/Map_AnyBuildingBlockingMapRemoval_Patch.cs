5a2c07299cc87d12b9431d5061aea728a51dd207
SokyranTheDragon <exploder1531@gmail.com>
2025-10-05 22:39:09 +0200

    Keep maps spawned if there's a gravjumper/gravhulk engine present

---
+++ Source/HarmonyPatches/Map_AnyBuildingBlockingMapRemoval_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(Map), nameof(Map.AnyBuildingBlockingMapRemoval), MethodType.Getter)]
+public class Map_AnyBuildingBlockingMapRemoval_Patch
+{
+    private static void Postfix(Map __instance, ref bool __result)
+    {
+        // If false, check if there's any of our own engines on the map to prevent the map from being removed.
+        if (!__result)
+        {
+            __result = __instance.listerThings.AnyThingWithDef(VGEDefOf.VGE_GravjumperEngine) ||
+                       __instance.listerThings.AnyThingWithDef(VGEDefOf.VGE_GravhulkEngine);
+        }
+    }
+}
