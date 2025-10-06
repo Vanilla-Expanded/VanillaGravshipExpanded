a33638674558295314f1c5e0327347a5469e22d0
SokyranTheDragon <exploder1531@gmail.com>
2025-10-07 00:14:34 +0200

    Add a feature where pawns will attempt to escape being squished by a gravship

---
+++ Source/HarmonyPatches/GravshipPlacementUtility_PlaceGravshipInMap_Patch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using RimWorld.Planet;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(WorldComponent_GravshipController), nameof(WorldComponent_GravshipController.InitiateLanding))]
+public static class GravshipPlacementUtility_PlaceGravshipInMap_Patch
+{
+    private static void Prefix(Gravship gravship, Map map, IntVec3 landingPos)
+    {
+        // We DON'T want to ever error on this, as this will break vanilla landing otherwise.
+        // Wrap in try/catch for extra safety to prevent any exception from slipping through.
+        try
+        {
+            const int maxSearchRadius = 10;
+
+            var positions = gravship.Terrains
+                .Select(x => x.Key + landingPos)
+                .Concat(gravship.Foundations.Select(x => x.Key + landingPos))
+                .ToHashSet();
+            var pawns = positions
+                .SelectMany(x => x.GetThingList(map))
+                .OfType<Pawn>()
+                .ToList();
+
+            foreach (var pawn in pawns)
+            {
+                if (!pawn.IsAnimal && Rand.Bool)
+                {
+                    var cell = CellFinder.StandableCellNear(pawn.Position, map, maxSearchRadius, x => !positions.Contains(x));
+                    if (cell.IsValid)
+                        pawn.Position = cell;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error($"[VGE] Exception caught trying to move pawns out of gravship landing area. Exception:\n{e}");
+        }
+    }
+}
