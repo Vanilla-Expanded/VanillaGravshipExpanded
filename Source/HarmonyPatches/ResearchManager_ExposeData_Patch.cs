8f810172415f9a1cc15d2e3fcacf70d0aee5855d
SokyranTheDragon <exploder1531@gmail.com>
2025-11-03 02:13:33 +0100

    Unlock basic gravtech as fallback on load if grav engine was inspected

---
+++ Source/HarmonyPatches/ResearchManager_ExposeData_Patch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(ResearchManager), nameof(ResearchManager.ExposeData))]
+public static class ResearchManager_ExposeData_Patch
+{
+    private static void Postfix()
+    {
+        if (Scribe.mode == LoadSaveMode.PostLoadInit && Find.ResearchManager.gravEngineInspected && !ResearchProjectDefOf.BasicGravtech.IsFinished)
+            Find.ResearchManager.FinishProject(ResearchProjectDefOf.BasicGravtech, doCompletionLetter: false);
+    }
+}
