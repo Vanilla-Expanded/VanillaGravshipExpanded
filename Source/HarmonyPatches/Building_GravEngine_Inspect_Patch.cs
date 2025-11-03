3d2c26a0b56451f4852fff2a298b69bb68b646ec
SokyranTheDragon <exploder1531@gmail.com>
2025-11-03 02:00:34 +0100

    Research basic gravtech on inspecting any grav engine

--- Source/HarmonyPatches/Building_GravEngine_Inspect_Patch.cs
+++ Source/HarmonyPatches/Building_GravEngine_Inspect_Patch.cs
@@ -7,9 +7,19 @@ namespace VanillaGravshipExpanded
     [HarmonyPatch(typeof(Building_GravEngine), nameof(Building_GravEngine.Inspect))]
     public static class Building_GravEngine_Inspect_Patch
     {
-        public static void Postfix(Building_GravEngine __instance)
+        public static void Prefix(out bool __state)
         {
+            __state = Find.ResearchManager.gravEngineInspected;
+        }
+
+        public static void Postfix(Building_GravEngine __instance, bool __state)
+        {
+            // Sanity check against re-inspecting a grav engine a second time
+            if (__state)
+                return;
+
             QuestUtility.SendQuestTargetSignals(__instance.Map.Parent.questTags, "Inspected", __instance.Named("SUBJECT"));
+            Find.ResearchManager.FinishProject(ResearchProjectDefOf.BasicGravtech);
         }
     }
 }
