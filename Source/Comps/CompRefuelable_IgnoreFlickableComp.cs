98ff9d4dac9461206039d7ab3f5e79a3dd2b0682
SokyranTheDragon <exploder1531@gmail.com>
2025-11-03 17:26:09 +0100

    Algae oxygenator always consumes oxyalgae

---
+++ Source/Comps/CompRefuelable_IgnoreFlickableComp.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+
+namespace VanillaGravshipExpanded;
+
+public class CompRefuelable_IgnoreFlickableComp : CompRefuelable
+{
+    public override void CompTick()
+    {
+        // Temporarily set the flickComp to null so the fuel is consumed even if flicked off.
+        // We could technically permanently set it to null in PostSpawnSetup, but pawns we
+        // need it for ShouldAutoRefuelNowIgnoringFuelPct getter, which would allow pawns
+        // to auto-refuel the refuelable despite it being wasteful - we only want to
+        // auto-refuel when flicked on so the resources are processed.
+        var comp = flickComp;
+        try
+        {
+            flickComp = null;
+            base.CompTick();
+        }
+        finally
+        {
+            flickComp = comp;
+        }
+    }
+}
